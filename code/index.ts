bc4e273234ec5e556404688ba1abfd8c512d6b36
chneau <charles63500@gmail.com>
2023-11-15 11:11:40 +0000

    Update dependencies and add .gitignore

--- code/index.ts
+++ code/index.ts
@@ -8,7 +8,7 @@ const tempFile = "default-dark-theme-vscode.json";
 const finalFile = "../themes/Saturated Dark Modern-color-theme.json";
 
 const start = new Date();
-const msSinceStartInMs = () => new Date().getTime() - start.getTime();
+const msSinceStartInMs = () => (new Date().getTime() - start.getTime()).toString().padStart(3, " ");
 console.log(`${msSinceStartInMs()}ms: Starting...`);
 
 // download the themes
